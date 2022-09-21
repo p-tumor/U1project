f20b5dd6f9343b1805eef9b7aad5978e14de9ad9
BT_4N2_01 <xyab@gmail.com>
2022-09-21 11:51:16 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void Main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Total Bill: ");
+        double bill = s.nextDouble();
+        System.out.println("Tip Percentage: ");
+        double tipPercent = s.nextDouble();
+        System.out.println("Number of People: ");
+        int numPpl = s.nextInt();
+
+        
+    }
+
+}
