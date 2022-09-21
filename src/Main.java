6039041e4142d5e15493badcf3ee236e5391c681
BT_4N2_01 <xyab@gmail.com>
2022-09-21 12:24:22 -0400

    job's done

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,20 @@ public class Main {
         double bill = s.nextDouble();
         System.out.print("Tip Percentage (w/o percent): ");
         double tipPercent = s.nextDouble();
+        tipPercent = tipPercent/100;
         System.out.print("Number of People: ");
         int numPpl = s.nextInt();
 
+        double tipAmt = bill*tipPercent;
+        double tipPerPerson = tipAmt/numPpl;
+        double totBill = bill+tipAmt;
+        double totBillPerPerson= totBill/numPpl;
 
+        System.out.println("---------------------------------------------------------");
+        System.out.printf("Total Bill: $%.2f\n", totBill);
+        System.out.printf("Amount Tipped: $%.2f\n", tipAmt);
+        System.out.printf("Tip Per Individual: $%.2f\n", tipPerPerson);
+        System.out.printf("Total Per Individual: $%.2f\n", totBillPerPerson);
     }
 
 }
